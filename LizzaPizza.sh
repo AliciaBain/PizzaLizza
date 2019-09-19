e49c6f891c9b8827e759fbcd15e230cfcdc927a9
Apronius <Aaron.Guanch@gmail.com>
2019-09-19 16:58:31 -0400

    Added Lizza Header

--- LizzaPizza.sh
+++ LizzaPizza.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 #This is our pizza place
+clear
+echo "         _______ _____ _____          |"
+echo "  |         |        /     /     /\     ______"
+echo "  |         |       /     /     /  \    |"
+echo "  |         |      /     /     /----\   |____"
+echo "  |         |     /     /     /      \       |"
+echo "  |_____ ___|___ /____ /____ /        \ _____| "
+
+echo "Motto: Probably better than nothing!"
+
+echo "About Us: We're 
 
 
