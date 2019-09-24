fa3c3c351eeb14e9c93f3f5b8d1026b84648ec9f
Apronius <Aaron.Guanch@gmail.com>
2019-09-23 21:02:23 -0400

    more updates, got the selection menu to work somewhat, working on prices

--- LizzaPizza.sh
+++ LizzaPizza.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-#Variables
+#Arrays
+#ansone=()
+
+#Variables-------------------------------------------------------------------------------
 FIRST=55
 SECOND=70
 THIRD=99
 
-function food
+PIZ1="Cheese Pizza"
+PIZ2="Vegan Pizza"
+PIZ3="Lizza's Special"
+
+#Functions------------------------------------------------------------------------------
+function food #Displays menu
 {
-clear
 echo "Menu:"
 echo "--------------------------------"
 
@@ -18,19 +25,37 @@ printf "1) Cheese Pizza - "$"55\n - Bread bits mixed into a pile of tomatoe sauc
 printf "2) Vegan Pizza - "$"70\n - A Whole Tomatoe\n"
 
 printf "3) Lizza's Special - "$"99\n - Hot melted cheese, comes with a metal straw to drink it with\n"
+echo ""
 }
 
-#This is our pizza place
+function order #Displays current order
+{
+printf "Your selection so far:\n"
+ if [ "$selection" = "1" ] || [ "$selection" = "Cheese Pizza"] || [ "$selection" = "cheese pizza"]
+ then
+	 echo "Food: $ansone($x) $PIZ1 "$"$(($FIRST+$ansone($x)))"
+else 
+echo ""
+fi
+}
 
-clear
-
-printf 'WELCOME TO OUR PIZZA PLACE\n'
+function title #Clears the screen then displays title
+{
+	clear
 echo "         _______ _____ _____          |"
 echo "  |         |        /     /     /\     ______"
 echo "  |         |       /     /     /  \    |"
 echo "  |         |      /     /     /----\   |____"
 echo "  |         |     /     /     /      \       |"
 echo "  |_____ ___|___ /____ /____ /        \ _____| "
+printf "\n"
+}
+
+
+#---------------------------------------------------------------------------------------
+
+
+title
 
 echo "Motto: I'm lovin' it"
 
@@ -40,36 +65,38 @@ sleep 5
 
 
 #Food menu selection
-
+title
 food
 
 read -p "Which item would you like? >> " selection
 
 while true
 do
-if [ "$selection" = "1" ] || [ "$selection" = "Cheese Pizza" ] || [ "$selection" = "c" ] 
+if [ "$selection" = "1" ] || [ "$selection" = "Cheese Pizza" ] || [ "$selection" = "C*" ]
  then
   read -p "You have selected Cheese Pizza. How many would you like? >> " ansone
-   if [ $ansone = * ]
+   if [ $ansone -lt 1 ]
     then 
      read -p "Please enter a valid number >> " ansone
-      fi
+      else 
+       echo "$ansone Cheese Pizza"	
+       break
+       fi
+       fi
+       done      
 
-        echo ""
-      echo "Would you like another Pizza?"
-       
-       read input
-        case $input in
+echo "Would you like another Pizza? [y or n]  "
+    read input
+       case $input in
         Y|y|yes|Yes|Y*)
-        food
-         read -p "Which item would you like? >> " selectiontwo;;
-        n|N|No|no|N*|n*)  
-         break
+         food
+          read -p "Which item would you like? >> " selectiontwo;;
+           n|N|No|no|N*|n*)  
+            break
           esac 
- else
-  read -p "Invalid selection. Please Select an item >> " selection
-fi
-done
+# else
+ # read -p "Invalid selection. Please Select an item >> " selection
+
 
 #function thing
 #{
@@ -77,13 +104,3 @@ done
 #y|Y|yes|Yes|y*|Y*
 #esac
 #}
-
-function taxes
-{
- A=.07
- SUM=$(($1 + $2))
- TAX=$(echo "$SUM*$A"|bc)
- REAL=$(echo "$SUM+$TAX"|bc)
- echo "Total: $"$REAL""
-}
-
